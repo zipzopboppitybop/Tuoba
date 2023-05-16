02dd86ec11d5dc97917563207affaa0b3d110eea
Brian Washington <bw49078@gmail.com>
2023-05-16 15:48:17 -0500

    added base for navbar

--- react-app/src/components/Navigation/index.js
+++ react-app/src/components/Navigation/index.js
@@ -6,17 +6,43 @@ import './Navigation.css';
 
 function Navigation({ isLoaded }) {
 	const sessionUser = useSelector(state => state.session.user);
-
+	const url = window.location.pathname
+	console.log(url)
 	return (
-		<ul>
-			<li>
-				<NavLink exact to="/">Home</NavLink>
+		<ul className='navbar'>
+			<li className='title'>
+				Tuoba
 			</li>
+			{url === "/" ? (
+				<li>
+					<NavLink className='fa current  fa-home' exact to="/"></NavLink>
+				</li>
+			) : (
+				<li>
+					<NavLink className='fa  fa-home' exact to="/"></NavLink>
+				</li>
+			)}
+			{url === "/following" ? (
+				<li>
+					<NavLink className='fa current  fas fa-book' exact to="/following"></NavLink>
+				</li>
+			) : (
+				<li>
+					<NavLink className='fa  fas fa-book' exact to="/following"></NavLink>
+				</li>
+			)}
+
 			{isLoaded && (
 				<li>
 					<ProfileButton user={sessionUser} />
 				</li>
 			)}
+			{sessionUser ? (
+				<li>
+					<button>hello</button>
+				</li>
+
+			) : (<></>)}
 		</ul>
 	);
 }

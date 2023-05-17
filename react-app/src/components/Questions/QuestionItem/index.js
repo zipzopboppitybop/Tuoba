ad420987acc270a5df235fdd9245ff55bd933b8e
Brian Washington <bw49078@gmail.com>
2023-05-17 17:44:46 -0500

    can now follow and unfollow other users

--- react-app/src/components/Questions/QuestionItem/index.js
+++ react-app/src/components/Questions/QuestionItem/index.js
@@ -5,14 +5,29 @@ import OpenModalButton from "../../OpenModalButton"
 import UpdateQuestion from "../UpdateQuestion";
 import { NavLink } from "react-router-dom";
 import './QuestionItem.css'
-import SingleQuesiton from "../SingleQuestion";
+import { getFollowsForUser } from "../../../store/follow";
+import { getAllQuestions } from "../../../store/question";
 
 const QuestionItem = ({ question }) => {
+    const dispatch = useDispatch();
     const currentUser = useSelector(state => state?.session?.user)
+    const follower = question?.owner?.followers?.find(id => id === currentUser?.id)
+
+    const onSubmitFollow = async (e) => {
+        e.preventDefault()
+
+        dispatch(getFollowsForUser(question?.userId))
+        dispatch(getAllQuestions())
+
+    }
+
 
     return (
         <div>
             {question?.owner?.username}
+            <div className="username-unfollow-follow">
+                {currentUser && follower && (currentUser?.id !== question?.userId) ? <button className="button-unfollow" onClick={onSubmitFollow}>unfollow</button> : currentUser && !follower && (currentUser?.id !== question?.userId) ? <button className="button-follow" onClick={onSubmitFollow}>Follow</button> : <></>}
+            </div>
             <NavLink question={question} className='question-link' to={`/questions/${question.id}`}>
                 {question?.content}
             </NavLink>
